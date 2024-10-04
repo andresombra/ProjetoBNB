f7ce6f221933e80630e5caa31850b18b6dc65772
andresombra <andresombra.net@gmail.com>
2024-10-04 16:52:02 -0300

    Domain criado

---
+++ ProjetoBNB/BNB.Domain/Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNB.Domain.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string? Endereco { get; set; }
+        public required string Email { get; set; }
+        public required string Telefone { get; set; }
+    }
+}
---
+++ ProjetoBNB/BNB.Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,16 @@
+﻿using BNB.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNB.Domain.Repositories
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente?> GetByIdAsync(int id);
+        Task AddAsync(Cliente cliente);
+        Task SaveChangesAsync();
+    }
+}
