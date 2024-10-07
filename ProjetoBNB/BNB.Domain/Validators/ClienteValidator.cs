75d89685c3471eb397d5b96963e5330d271e1732
andresombra <andresombra.net@gmail.com>
2024-10-07 11:22:53 -0300

    Refatorado request

--- ProjetoBNB/BNB.Domain/Validators/ClienteValidator.cs
+++ ProjetoBNB/BNB.Domain/Validators/ClienteValidator.cs
@@ -11,8 +11,21 @@ namespace BNB.Domain.Validators
                 .NotEmpty().WithMessage("O nome é obrigatório.")
                 .Length(2, 100).WithMessage("O nome deve ter entre 2 e 100 caracteres.");
 
+            RuleFor(cliente => cliente.Nome).NotEqual("string")
+               .WithMessage("Preencher campo nome.");
+
+            RuleFor(cliente => cliente.Endereco).NotEqual("string")
+               .WithMessage("Preencher campo endereco.");
+
+            RuleFor(cliente => cliente.Email).NotEqual("string")
+               .WithMessage("Preencher campo e-mail.");
+
+            RuleFor(cliente => cliente.Telefone).NotEqual("string")
+               .WithMessage("Preencher campo telefone.");
+
             RuleFor(cliente => cliente.Email)
                 .EmailAddress().WithMessage("E-mail inválido.");
         }
     }
 }
+
